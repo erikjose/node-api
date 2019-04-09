e874f2be7c45d4b92c8d7597fbcf098a925ced1c
Erik José Silva <erik@wizzer.com.br>
2019-04-09 14:45:07 -0300

    Process env URL

--- src/server.js
+++ src/server.js
@@ -33,7 +33,7 @@ app.use('/files', express.static(path.resolve(__dirname, '..', 'temp')))
 // Routes
 app.use(require('./routes'));
 
-server.listen(3001);
+server.listen(process.env.PORT || 3001);
 
 
 // mongodb+srv://node-api:<password>@cluster0-cegcb.mongodb.net/test?retryWrites=true
